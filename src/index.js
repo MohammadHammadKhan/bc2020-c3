c994a69b1fed8436a8e2a04e5369f3556ecab597
Mohammad Hammad Khan <66447399+MHKDESIGNS@users.noreply.github.com>
2021-04-30 17:18:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,21 +6,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// function Room(props){
-//   let [isLit,setLit]=React.useState(false)
-//   let [count,setCount]=React.useState(10)
+ function Room(props){
+  let [isLit,setLit]=React.useState(false)
+  let [count,setCount]=React.useState(10)
 
-//   return <div className={isLit? 'light':'dark'}>Room is {isLit? 'light':'dark'} count is {count}
-//   <br/>
-//          <button onClick={()=>setLit(!isLit)}>ON-OFF</button>
-//          <button onClick={()=>setCount(count+1)}>+</button>
-//          <button onClick={()=>setCount(count-1)}>-</button>
-//          </div>
+   return <div className={isLit? 'light':'dark'}>Room is {isLit? 'light':'dark'} count is {count}
+   <br/>
+          <button onClick={()=>setLit(!isLit)}>ON-OFF</button>
+         <button onClick={()=>setCount(count+1)}>+</button>
+          <button onClick={()=>setCount(count-1)}>-</button>
+         </div>
 
-// }
+}
+ReactDOM.render(<div>
+    <Room/>
+    <Room/>
+    <Room/>
+    <Room/>
+    </div>,document.getElementById('root'))
 
-
-ReactDOM.render(<App name='MHKkk' age={35}/>,document.getElementById('root'))
+//ReactDOM.render(<App name='MHKkk' age={35}/>,document.getElementById('root'))
 // const Gate=({isopen})=>isopen? 'Open':'Closed'
 // const Hi=({name,age})=>{
 //   return <div>Hello World <strong>{name}</strong> age is {age}</div>
